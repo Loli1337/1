4a3973f54a175ba93c9500a7fbddcdbc63a175da
Loli <89763268+Loli1337@users.noreply.github.com>
2023-05-22 02:10:09 -0700

    Create 2.sh

---
+++ 2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Set log file path
+logFile="/var/log/3cx_automation.log"
+
+# Show script name and variables
+echo Ran script $0 with parameters: >> "${logFile}"
+echo CallerID: $1, DIDNumber: $2, CallerInput: $3, CallerInpu: $4 >> "${logFile}"
+echo __________________________________________________ >> "${logFile}"
+
+# Return exit code
+exit 3
