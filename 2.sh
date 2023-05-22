7dc7064db6c81d716484f1e9dd80c5613eb27868
Loli <89763268+Loli1337@users.noreply.github.com>
2023-05-22 02:12:43 -0700

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -5,7 +5,7 @@ logFile="/var/log/3cx_automation.log"
 
 # Show script name and variables
 echo Ran script $0 with parameters: >> "${logFile}"
-echo CallerID: $1, DIDNumber: $2, CallerInput: $3 >> "${logFile}"
+echo CallerID: $1, DIDNumber: $2, CallerInput: $3, CallerInpu: $4 >> "${logFile}"
 echo __________________________________________________ >> "${logFile}"
 
 # Return exit code
