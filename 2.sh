5bf8541d2775f4ea7360c0584a515d8437f306ae
Loli <89763268+Loli1337@users.noreply.github.com>
2023-05-22 03:45:24 -0700

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -1,10 +1,12 @@
-#!/bin/python3
-python3 /home/100.py
+#!/bin/bash
+
+# Set log file path
+logFile="/var/log/3cx_automation.log"
 
 # Show script name and variables
-#echo Ran script $0 with parameters: >> "${logFile}"
-#echo CallerID: $1, DIDNumber: $2, CallerInput: $3, CallerInpu: $4 $5 $6 $7 $8 $9 >> "${logFile}"
-#echo __________________________________________________ >> "${logFile}"
-#python3 path/to/your/python/file.py
+echo Ran script $0 with parameters: >> "${logFile}"
+echo CallerID: $1, DIDNumber: $2, CallerInput: a $3 b $4 c $5 d $6 e $7 f $8 g $9 >> "${logFile}"
+echo _____________________________________________ >> "${logFile}"
+
 # Return exit code
 exit 3
